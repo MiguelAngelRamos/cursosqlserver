2008625fcdefd1c19012c74034fb03a736df8856
Miguel Ramos <return.infox@gmail.com>
2026-08-10 11:52:38 -0400

    actualizado

--- clase-01/consultas.sql
+++ clase-01/consultas.sql
@@ -38,3 +38,72 @@ ORDER BY PrecioConDescuento;
 -- COMO QUEDARIA LA CONSULTA?
 
 
+SELECT 	
+	NombreProducto,
+	PrecioUnitario AS PrecioConDescuento
+FROM dbo.Productos
+WHERE PrecioUnitario > 100000.00
+ORDER BY PrecioConDescuento;
+
+/*
+ QUE ES NULL
+
+ NULL representa la AUSENCIA de un valor. Significa "no se sabe" o "no aplica".
+
+ NULL no es cero.
+ NULL no es una cadena vacia.
+ NULL no es un espacio en blanco.
+
+ En la tabla Clientes, Hector Ramirez Vega tiene NULL en CorreoElectronico.
+ Eso no significa que su correo sea vacio: significa que la empresa no sabe cual es.
+*/
+
+SELECT * FROM Clientes;
+
+-- Cuantos Clientes hay sin correo 
+
+SELECT 
+ NombreCompleto,
+ CorreoElectronico
+FROM Clientes
+WHERE CorreoElectronico IS NULL;
+
+-- Cuantos Clientes hay sin Telefono
+SELECT 
+ NombreCompleto,
+ NumeroTelefono
+FROM Clientes
+WHERE NumeroTelefono IS NULL;
+
+-- UNIR TODO
+
+SELECT 
+ NombreCompleto,
+ CorreoElectronico,
+ NumeroTelefono,
+ CASE
+	WHEN CorreoElectronico IS NULL AND NumeroTelefono IS NULL THEN 'Sin correo ni telefono'
+	WHEN CorreoElectronico IS NULL                            THEN 'Sin correo'
+	WHEN NumeroTelefono    IS NULL                            THEN 'Sin telefono'
+	ELSE 'Datos completos'
+ END AS DatoFaltante
+FROM Clientes;
+
+
+SELECT 
+ NombreCompleto,
+ CorreoElectronico,
+ NumeroTelefono,
+ CASE
+	WHEN CorreoElectronico IS NULL AND NumeroTelefono IS NULL THEN 'Sin correo ni telefono'
+	WHEN CorreoElectronico IS NULL                            THEN 'Sin correo'
+	WHEN NumeroTelefono    IS NULL                            THEN 'Sin telefono'
+	ELSE 'Datos completos'
+ END AS DatoFaltante
+FROM Clientes
+WHERE CorreoElectronico IS NULL OR NumeroTelefono IS NULL;
+
+/*
+La regla que vale la pena dejar grabada WHERE decide que filas ves, CASE decide que texto muestra cada columna,
+son responsabilidades separadas
+*/
